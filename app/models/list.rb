d3da5fe0fc3882683e55ed30d4c8712b808a0b11
Mia OConnell <miaxoconnell@gmail.com>
2024-08-09 16:34:19 +0900

    testing heroku

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,4 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   has_one_attached :photo
-  validates :photo, presence: true
 end
